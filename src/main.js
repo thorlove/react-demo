2d343514b6aa82013ccb44963ff916b0f7d31f04
Robin <haibing.tian@gmail.com>
2016-05-20 18:00:57 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { App } from '.app/App';
+import { App } from './app/App';
+import './styles/common.css';
+import './styles/font.css';
 
-render(<App />, document.getElementById('root'));
+render(<div><i className="material-icons md-36"></i> <App /></div>, document.getElementById('root'));
