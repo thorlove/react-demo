722abb5dcea172170532cd6f9b41dac1b9ce86a2
Robin <haibing.tian@gmail.com>
2016-05-20 18:29:29 +0800

    init

--- src/main.js
+++ src/main.js
@@ -4,4 +4,4 @@ import { App } from './app/App';
 import './styles/common.css';
 import './styles/font.css';
 
-render(<div><i className="material-icons md-36">face</i> <App /></div>, document.getElementById('root'));
+render(<div><i className="material-icons md-36">alarm</i> <App /></div>, document.getElementById('root'));
