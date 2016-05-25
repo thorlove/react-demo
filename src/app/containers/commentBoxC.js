3e51846ab81e2297c37053f1172687e0327e91cc
Robin <haibing.tian@gmail.com>
2016-05-25 19:43:57 +0800

    添加查询和过虑功能

--- src/app/containers/commentBoxC.js
+++ src/app/containers/commentBoxC.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import * as actions from '../actions/CommentBoxA.js';
-import {CommentFilter,CommentSearch,CommentList,Comment,CommentForm} from '../components/CommentBox.js';
+import {CommentFilter, CommentSearch, CommentList, Comment, CommentForm} from '../components/CommentBox.js';
 
 
-
+class CommentBox extends Component {
+    render() {
+        return (
+            <div className="commentBox">
+                <CommentSearch onChange={param => this.searchComment(param) }/>
+                <h3>评论列表</h3>
+                <CommentFilter onClick={filter => this.filterComment(filter) }/>
+                <CommentList comments = {this.state.comments}/>
+                <CommentForm onSubmit = {comment => this.saveCommentServ(comment) }/>
+            </div>
+        );
+    }
 
