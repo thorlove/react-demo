f93c5ee57067c4ffa2eabf7861e7ed3db5b2d0d1
Robin <haibing.tian@gmail.com>
2016-06-01 19:36:27 +0800

    delete

--- src/app/utils/creatorAction.js
+++
@@ -1,9 +0,0 @@
-export default function creatorAction(type, ...argNames) {
-  return function(...args) {
-    let action = { type }
-    argNames.forEach((arg, index) => {
-      action[argNames[index]] = args[index]
-    })
-    return action
-  }
-}
